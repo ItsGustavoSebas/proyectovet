49a9e69005532355d44bbe6ac4c0d19cc9cfb51f
ItsGustavoSebas <gusta25demarzo@gmail.com>
2023-10-22 19:37:57 -0400

    generalizacion

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -35,6 +35,7 @@ class CreateNewUser implements CreatesNewUsers
             'ci' => $input['ci'],
             'telefono' => $input['telefono'],
             'direccion' => $input['direccion'],
+            'rol' => 'cliente',
             'password' => Hash::make($input['password']),
         ]);
     }
