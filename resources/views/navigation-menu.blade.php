a35695aa6e1b5bde1d7b36e014fc41a789a39303
lFerDanl <fcossio100@gmail.com>
2023-10-31 00:30:37 -0400

    Lista_Paquetes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-500">
     <!-- Primary Navigation Menu -->
-    <div class="container mx-auto flex flex-wrap p-6 pl-10 flex-col md:flex-row items-center"> {{-- "max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" --}}
+    <div class="px-4 p-6"> {{-- "container mx-auto flex flex-wrap p-6 pl-10 flex-col md:flex-row items-center"--}} {{-- "max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" --}}
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
