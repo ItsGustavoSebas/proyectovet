6693318de335dd6ed4b3575a5cb821c68430df2d
CarlosRomagera <carlosromagera2002@gmail.com>
2023-11-13 20:03:27 -0400

    Corrección Menú

---
+++ database/migrations/2023_11_13_151905_create_receta_medica_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receta_medica', function (Blueprint $table) {
+            $table->id();
+            $table->date('FechaEmision');
+            $table->integer('ID_Consulta')->foreign('ID_Consulta')->references('id')->on('consulta');
+            $table->integer('ID_Empleado')->foreign('ID_Empleado')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receta_medica');
+    }
+};
