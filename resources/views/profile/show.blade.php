73d8a1cfd57a34494d0f4c1f3dc6def9b5693565
lFerDanl <fcossio100@gmail.com>
2023-12-11 17:05:28 -0400

    Parche_Perfil2

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,9 +7,9 @@
         </h2>
     </x-slot>
 
-    <div class="mx-auto">
-        <div style="background: #f9fafa" class= "mx-40 mt-10">
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="mx-auto sm:w-full md:w-3/4 lg:w-1/2 xl:w-1/3">
+        <div style="background: #f9fafa" class="mx-4 md:mx-10 my-4 md:my-0 p-4 md:p-10">
+            <div class="max-w-7xl mx-auto">
                 @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                     @livewire('profile.update-profile-information-form')
     
@@ -17,33 +17,25 @@
                 @endif
     
                 @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                    <div class="mt-10 sm:mt-0">
+                    <div class="mt-4 sm:mt-6">
                         @livewire('profile.update-password-form')
                     </div>
     
                     <x-section-border />
                 @endif
     
-                {{-- @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                    <div class="mt-10 sm:mt-0">
-                        @livewire('profile.two-factor-authentication-form')
-                    </div>
-    
-                    <x-section-border />
-                @endif --}}
-    
-                <div class="mt-10 sm:mt-0">
+                <div class="mt-4 sm:mt-6">
                     @livewire('profile.logout-other-browser-sessions-form')
                 </div>
     
                 @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
                     <x-section-border />
     
-                    <div class="mt-10 sm:mt-0">
+                    <div class="mt-4 sm:mt-6">
                         @livewire('profile.delete-user-form')
                     </div>
                 @endif
             </div>
         </div>
-    </div>
+    </div>    
 </x-app-layout>
